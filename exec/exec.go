a16fc569541cc57bc5eaf58c0736b30be8a9f6ff
Hong Shick Pak <hong@hspak.com>
2021-03-25 15:10:01 -0700

    Allow failures due to lock contention fail successfully
    This will enable tracking actual errors like failed commands or other
    misc dynamodb errors. Lock contention is an expected behavior and we
    shouldn't be treating those as errors.
    
    I also updated the `Run` function to always fallback to returning the
    error from the command execution and not exiting early.
    
    I also stuck in a strings.TrimSpace.

--- exec/exec.go
+++ exec/exec.go
@@ -76,19 +76,19 @@ func (d *Exec) Run(tableName string, keyName string, command string, sleepStartR
 	logrus.WithFields(logrus.Fields{
 		"command": command,
 	}).Info("Executing command")
-	out, err := runExec(command)
+	out, cmdErr := runExec(command)
+
+	// Remove the trailing newline (and any other extraneous whitespace)
+	// so they don't pollute the log output.
+	trimedOutput := strings.TrimSpace(out)
 
 	// Always print output, regardless of error
-	lines := strings.Split(out, "\n")
+	lines := strings.Split(trimedOutput, "\n")
 	for _, l := range lines {
 		logrus.WithFields(logrus.Fields{
 			"command": command,
 			"line":    l,
 		}).Info("Command output")
-	}
-
-	if err != nil {
-		return err
 	}
 
 	if holdLockBy > 0 {
@@ -105,7 +105,9 @@ func (d *Exec) Run(tableName string, keyName string, command string, sleepStartR
 	}
 	logrus.Info("Lock released")
 
-	return nil
+	// If there were no locking errors,
+	// fallback to returning the error from the command.
+	return cmdErr
 }
 
 // RunCommand executes the command.
